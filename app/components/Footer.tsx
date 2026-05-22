2bf7ef44d13c5029300780467b18c80cc591df7b
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-05-22 12:49:54 -0300

    fix: point footer links to specific sections

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -15,31 +15,35 @@ export default function Footer() {
       </div>
       <div>
         <h4>Soluções</h4>
-        <a href="/solucoes">Todas as soluções</a>
-        <a href="/solucoes#solucoes-lista">Arquitetura e desenvolvimento</a>
-        <a href="/solucoes#solucoes-lista">IA e automações</a>
-        <a href="/solucoes#solucoes-lista">Sustentação</a>
+        <a href="/solucoes#cases-solucoes">Todas as soluções</a>
+        <a href="/solucoes#diagnostico-arquitetura">Diagnóstico e arquitetura</a>
+        <a href="/solucoes#sites-plataformas">Sites e plataformas</a>
+        <a href="/solucoes#wordpress-avancado">WordPress avançado</a>
+        <a href="/solucoes#ia-automacoes">IA e automações</a>
+        <a href="/solucoes#sustentacao-evolucao">Sustentação</a>
       </div>
       <div>
         <h4>Portfólio</h4>
-        <a href="/portfolio">Todos os projetos</a>
+        <a href="/portfolio#cases">Todos os projetos</a>
         <a href="/portfolio/tehkne-flow">Tehkné Flow</a>
         <a href="/portfolio/vacina-one">Vacina One</a>
         <a href="/portfolio/unti-digital">UNTI Digital</a>
+        <a href="/portfolio#contato-form">Quero um case parecido</a>
       </div>
       <div>
         <h4>Tehkné OS</h4>
-        <a href="/metodo">Método GIP</a>
-        <a href="/tehkne-os">Tehkné OS</a>
-        <a href="/tehkne-os#agentes">Agentes de IA</a>
-        <a href="/tehkne-os#governanca">Governança</a>
+        <a href="/metodo#fluxo-gip">Método GIP</a>
+        <a href="/tehkne-os#modulos">Módulos do OS</a>
+        <a href="/tehkne-os#camadas">Camadas operacionais</a>
+        <a href="/tehkne-os#aplicacoes">Para quem serve</a>
+        <a href="/tehkne-os#inteligencia">Inteligência operacional</a>
       </div>
       <div>
         <h4>Contato</h4>
-        <a href="/contato">Página de contato</a>
+        <a href="/contato#contato-form">Página de contato</a>
         <a href={diagnosticHref} target="_blank" rel="noreferrer">{WHATSAPP_DISPLAY}</a>
         <a href="mailto:contato@tehkne.com.br">contato@tehkne.com.br</a>
-        <a>Campinas, SP • Brasil</a>
+        <a href="/contato#localizacao">Campinas, SP • Brasil</a>
       </div>
     </footer>
   );
