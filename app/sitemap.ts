4af8e418fa87c61399c2335ce42d86cb07ec8034
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-05-22 22:25:14 -0300

    feat: include blog routes in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,9 @@
 import type { MetadataRoute } from 'next';
+import { blogPosts } from './blog/blog-data';
 import { portfolioCases } from './portfolio/portfolio-data';
 
 const siteUrl = 'https://tehkne.com';
-const staticPages = ['/', '/sobre', '/solucoes', '/servicos', '/portfolio', '/metodo', '/tehkne-os', '/contato'];
+const staticPages = ['/', '/sobre', '/solucoes', '/servicos', '/portfolio', '/metodo', '/tehkne-os', '/blog', '/contato'];
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date().toISOString();
@@ -12,10 +13,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: now
   }));
 
+  const blogUrls = blogPosts.map((post) => ({
+    url: `${siteUrl}/blog/${post.slug}`,
+    lastModified: post.date
+  }));
+
   const portfolioUrls = portfolioCases.map((project) => ({
     url: `${siteUrl}/portfolio/${project.slug}`,
     lastModified: now
   }));
 
-  return [...staticUrls, ...portfolioUrls];
+  return [...staticUrls, ...blogUrls, ...portfolioUrls];
 }
