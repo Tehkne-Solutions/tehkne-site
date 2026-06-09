60f1b22a3128883273192741c99e47fd0a5c5e92
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-06-09 19:13:55 -0300

    feat(blog): include system integration pillar posts

--- app/blog/all-blog-posts.ts
+++ app/blog/all-blog-posts.ts
@@ -2,8 +2,9 @@ import { blogPosts, type BlogPost } from './blog-data';
 import { newBlogPosts } from './blog-new-posts';
 import { seoPillarBlogPosts } from './blog-seo-pillar-posts';
 import { seoPillarBlogPosts2 } from './blog-seo-pillar-posts-2';
+import { seoPillarBlogPosts3 } from './blog-seo-pillar-posts-3';
 
-export const allBlogPosts: BlogPost[] = [...seoPillarBlogPosts2, ...seoPillarBlogPosts, ...newBlogPosts, ...blogPosts];
+export const allBlogPosts: BlogPost[] = [...seoPillarBlogPosts3, ...seoPillarBlogPosts2, ...seoPillarBlogPosts, ...newBlogPosts, ...blogPosts];
 
 export function getAllBlogPost(slug: string) {
   return allBlogPosts.find((post) => post.slug === slug);
