907dbe6c8f004c90ae15f24d8597cee723d6f45b
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-06-09 18:18:41 -0300

    feat(blog): include SEO pillar posts in blog library

--- app/blog/all-blog-posts.ts
+++ app/blog/all-blog-posts.ts
@@ -1,7 +1,8 @@
 import { blogPosts, type BlogPost } from './blog-data';
 import { newBlogPosts } from './blog-new-posts';
+import { seoPillarBlogPosts } from './blog-seo-pillar-posts';
 
-export const allBlogPosts: BlogPost[] = [...newBlogPosts, ...blogPosts];
+export const allBlogPosts: BlogPost[] = [...seoPillarBlogPosts, ...newBlogPosts, ...blogPosts];
 
 export function getAllBlogPost(slug: string) {
   return allBlogPosts.find((post) => post.slug === slug);
