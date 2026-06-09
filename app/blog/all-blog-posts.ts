237acc533191166f5412231e607b60d3c890662c
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-06-09 20:56:49 -0300

    feat(blog): include digital MVP pillar posts

--- app/blog/all-blog-posts.ts
+++ app/blog/all-blog-posts.ts
@@ -4,8 +4,9 @@ import { seoPillarBlogPosts } from './blog-seo-pillar-posts';
 import { seoPillarBlogPosts2 } from './blog-seo-pillar-posts-2';
 import { seoPillarBlogPosts3 } from './blog-seo-pillar-posts-3';
 import { seoPillarBlogPosts4 } from './blog-seo-pillar-posts-4';
+import { seoPillarBlogPosts5 } from './blog-seo-pillar-posts-5';
 
-export const allBlogPosts: BlogPost[] = [...seoPillarBlogPosts4, ...seoPillarBlogPosts3, ...seoPillarBlogPosts2, ...seoPillarBlogPosts, ...newBlogPosts, ...blogPosts];
+export const allBlogPosts: BlogPost[] = [...seoPillarBlogPosts5, ...seoPillarBlogPosts4, ...seoPillarBlogPosts3, ...seoPillarBlogPosts2, ...seoPillarBlogPosts, ...newBlogPosts, ...blogPosts];
 
 export function getAllBlogPost(slug: string) {
   return allBlogPosts.find((post) => post.slug === slug);
