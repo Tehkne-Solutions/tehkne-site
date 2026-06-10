ef420a5dd1dfce5de4a76c8bd66f12add951ce92
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-06-10 14:53:15 -0300

    feat(blog): include advanced WordPress pillar posts

--- app/blog/all-blog-posts.ts
+++ app/blog/all-blog-posts.ts
@@ -7,8 +7,9 @@ import { seoPillarBlogPosts4 } from './blog-seo-pillar-posts-4';
 import { seoPillarBlogPosts5 } from './blog-seo-pillar-posts-5';
 import { seoPillarBlogPosts6 } from './blog-seo-pillar-posts-6';
 import { seoPillarBlogPosts7 } from './blog-seo-pillar-posts-7';
+import { seoPillarBlogPosts8 } from './blog-seo-pillar-posts-8';
 
-export const allBlogPosts: BlogPost[] = [...seoPillarBlogPosts7, ...seoPillarBlogPosts6, ...seoPillarBlogPosts5, ...seoPillarBlogPosts4, ...seoPillarBlogPosts3, ...seoPillarBlogPosts2, ...seoPillarBlogPosts, ...newBlogPosts, ...blogPosts];
+export const allBlogPosts: BlogPost[] = [...seoPillarBlogPosts8, ...seoPillarBlogPosts7, ...seoPillarBlogPosts6, ...seoPillarBlogPosts5, ...seoPillarBlogPosts4, ...seoPillarBlogPosts3, ...seoPillarBlogPosts2, ...seoPillarBlogPosts, ...newBlogPosts, ...blogPosts];
 
 export function getAllBlogPost(slug: string) {
   return allBlogPosts.find((post) => post.slug === slug);
